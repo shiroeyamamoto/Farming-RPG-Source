86f86d6e8f13a7748b701b477d642fd34d6d6408
Khang Duong <khangvngm@gmail.com>
2023-07-24 12:10:36 +0700

    _5.2_Player_Basic@Player_Class_and_Abstract_Singleton_Class

---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonobehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    //kiểm tra Singleton đã tồn tại chưa, nếu chưa tồn tại thì tạo, còn nếu có rồi thì xóa đối tượng truyền vào
+    //Điều này đảm bảo rằng trong khi chạy hệ thống chỉ có duy nhất 1 Singleton tồn tại trong mọi thời điểm
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
