44c5e70b36091700e00bf192004de7d606db8970
Khang Duong <khangvngm@gmail.com>
2023-08-02 21:58:27 +0700

    _6.5_Game_Scenes&Tiles_Maps@Add_Cinemachine_Confiner

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+    /// <summary>
+    /// Switch va chạm giúp cinemachine có thể xác định được các cạnh của màn hình
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        // Sử dụng polygon collision với tag name "BoundsFiner" đã tạo để prevent(ngăn) cinemachine going beyond the scene
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        //Dọn cache khi có thay đổi confiner 
+
+        cinemachineConfiner.InvalidatePathCache();
+
+    }
+}
